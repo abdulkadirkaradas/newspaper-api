4857cce3f1444ef8162e3f9a923f88d60bb29c76
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-17 19:03:57 +0300

    Add Login, Users and News controllers

---
+++ app/Http/Controllers/v1/Users/UsersController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\v1\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    //
+}
--- app/Http/Middleware/CheckAuthorization.php
+++ app/Http/Middleware/CheckAuthorization.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\UserAuthTokens;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +20,26 @@ class CheckAuthorization
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!$request->header('auth-token')) {
+            return response()->json([
+                'status' => UNAUTHORIZED,
+                'message' => UNAUTHORIZED_ACCESS
+            ]);
+        }
+
+        $authToken = $request->header('auth-token');
+
+        $user = UserAuthTokens::where('token', $authToken)->first();
+
+        if (!$user) {
+            return response()->json([
+                'status' => UNAUTHORIZED,
+                'message' => UNAUTHORIZED_ACCESS
+            ]);
+        }
+
+        $request['user'] = $user;
+
         return $next($request);
     }
 }
