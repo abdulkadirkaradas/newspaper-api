97c827bda0b8191704a433e1e1ebfee67a62909b
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-17 18:31:20 +0300

    Add UserWarnings model and migration files

---
+++ app/Models/UserWarnings.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserWarnings extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = "user_warnings";
+
+    protected $fillable = [
+        "message",
+        "warning_level",
+        "user_id",
+    ];
+}
---
+++ database/migrations/2024_05_14_190410_create_user_warnings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_warnings', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('message');
+            $table->integer('warning_level')->default(0);
+            $table->foreignUuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_warnings');
+    }
+};
