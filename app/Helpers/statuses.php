b5d47c28e95a788efad1b58146d3c9f6c8d46aba
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-18 16:10:22 +0300

    Add bad_request constant

--- app/Helpers/statuses.php
+++ app/Helpers/statuses.php
@@ -4,6 +4,7 @@ const FAIL = 0;
 const SUCCESS = 200;
 const NO_CONTENT = 204;
 const MOVED_PERMANENTLY = 301;
+const BAD_REQUEST = 400;
 const UNAUTHORIZED = 401;
 const FORBIDDEN = 403;
 const NOT_FOUND = 404;
