233f80810e2ff99be6426c62aa03e45717c0d933
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-19 14:10:37 +0300

    Add auth-token, expire_date and role checks

--- app/Http/Middleware/CheckAuthorization.php
+++ app/Http/Middleware/CheckAuthorization.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Helpers\UserRoles;
+use App\Models\UserAuthTokens;
+use App\Models\Users;
+use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,6 +24,47 @@ class CheckAuthorization
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // This condition will be removed in the future, now just added for the test purposes
+        if (str_contains($request->url(), '/register')) {
+            return $next($request);
+        }
+
+        if (!$request->header('auth-token')) {
+            return response()->json($this->errorMessage(UNAUTHORIZED, UNAUTHORIZED_ACCESS));
+        }
+
+        $authToken = $request->header('auth-token');
+
+        $auth = UserAuthTokens::where('token', $authToken)->first();
+        $user = Users::find($auth->user_id);
+
+        $userRoles = [
+            UserRoles::$admin,
+            UserRoles::$mod,
+            UserRoles::$writer
+        ];
+
+        if (!in_array($user->role_id, $userRoles)) {
+            return response()->json($this->errorMessage(UNAUTHORIZED, UNAUTHORIZED_ACCESS));
+        }
+
+        $expireDateTS = strtotime($auth->expire_date);
+        $currentTS = time();
+
+        if ($expireDateTS < $currentTS) {
+            return response()->json($this->errorMessage(UNAUTHORIZED, SESSION_EXPIRED));
+        }
+
+        $request['user'] = $user;
+
         return $next($request);
+    }
+
+    private function errorMessage(int $status, string $message): array
+    {
+        return [
+            "status" => $status,
+            "message" => $message
+        ];
     }
 }
