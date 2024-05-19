9c77a897d3342b5bdcc890caaae97f846988e015
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-19 16:28:32 +0300

    Adjust register middleware usage for the register route

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\v1\RegisterController;
+use App\Http\Middleware\CheckAuthentication;
 use App\Http\Middleware\CheckAuthorization;
+use App\Http\Middleware\RegisterMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1/auth')->middleware([CheckAuthorization::class])->group(function () {
-    Route::post('/register', [RegisterController::class, 'register']);
+Route::prefix('v1/auth')->middleware([CheckAuthentication::class/*, CheckAuthorization::class*/])->group(function () {
+
+    Route::middleware([RegisterMiddleware::class])->group(function () {
+        Route::post('/register', [RegisterController::class, 'register']);
+    });
 });
