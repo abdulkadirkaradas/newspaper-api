4d7de39c3819d77f1f93463d21e0d5a7fa3cfe9f
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-20 16:35:05 +0300

    Adjust Users model to use JWT

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class Users extends Model
+class Users extends Authenticatable implements JWTSubject
 {
     use HasFactory, HasUuids;
 
@@ -21,6 +22,46 @@ class Users extends Model
         "email",
         "password",
         "blocked",
-        "role_id"
+        "role_id",
+        "remember_token"
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed'
+    ];
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
