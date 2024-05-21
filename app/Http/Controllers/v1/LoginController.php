04297a445775c2d54bf41e08220624a70a88c652
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-21 14:42:01 +0300

    Add login function to the LoginController

--- app/Http/Controllers/v1/LoginController.php
+++ app/Http/Controllers/v1/LoginController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers\v1;
 
+use App\Validators\UserLoginValidator;
 use App\Http\Controllers\Controller;
+use App\Models\UserAuthTokens;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function login(Request $request) {
+        $validated = UserLoginValidator::validate($request);
 
+        if (gettype($validated) === 'array' && isset($validated['status']) && $validated['status'] === BAD_REQUEST) {
+            return $validated;
+        }
+
+        $token = UserAuthTokens::where('user_id', $request->user->id)->first();
+
+        if (!$token) {
+            return response()->json([
+                'status' => UNAUTHORIZED,
+                'message' => UNAUTHORIZED_ACCESS,
+            ], 401);
+        }
+
+        return response()->json([
+                'status' => SUCCESS,
+                'authorisation' => [
+                    'token' => $token->token,
+                    'type' => 'bearer',
+                ]
+            ]);
     }
 }
