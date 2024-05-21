88376cca13c3f38fe08bdcad1818dd2aeabeee41
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-21 14:42:28 +0300

    Add auth/login route

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,23 @@
 <?php
 
+use App\Http\Controllers\v1\LoginController;
 use App\Http\Controllers\v1\RegisterController;
 use App\Http\Middleware\CheckAuthentication;
 use App\Http\Middleware\CheckAuthorization;
+use App\Http\Middleware\LoginMiddleware;
 use App\Http\Middleware\RegisterMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1/auth')->middleware([CheckAuthentication::class/*, CheckAuthorization::class*/])->group(function () {
+Route::prefix('v1')->middleware([CheckAuthentication::class/*, CheckAuthorization::class*/])->group(function () {
 
-    Route::middleware([RegisterMiddleware::class])->group(function () {
-        Route::post('/register', [RegisterController::class, 'register']);
+    // Authentication Routes
+    Route::prefix('auth')->group(function () {
+        Route::middleware([RegisterMiddleware::class])->group(function () {
+            Route::post('/register', [RegisterController::class, 'register']);
+        });
+
+        Route::middleware([LoginMiddleware::class])->group(function () {
+            Route::post('/login', [LoginController::class, 'login']);
+        });
     });
 });
