143be3a6a26fbd96e76df1adbc7eaf5369eae9e5
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-22 16:07:11 +0300

    Remove blank comments

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -26,21 +26,11 @@ class Users extends Authenticatable implements JWTSubject
         "remember_token"
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'password' => 'hashed'
     ];
