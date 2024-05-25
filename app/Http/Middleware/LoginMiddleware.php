58c895ed45de714f1012e00cfca5998a012afc19
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-25 16:51:15 +0300

    Improve error handling and token check

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Models\User;
+use App\Models\UserAuthTokens;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -17,23 +18,16 @@ class LoginMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!$request->bearerToken()) {
-            return response()->json($this->errorMessage(UNAUTHORIZED, UNAUTHORIZED_ACCESS));
-        }
-
-        if ($request->bearerToken() &&
-            (str_contains($request->route()->getActionName(), 'refreshAuthToken') || str_contains($request->route()->getActionName(), 'logout'))) {
+        if (str_contains($request->route()->getActionName(), 'refreshAuthToken') || str_contains($request->route()->getActionName(), 'logout')) {
             return $next($request);
         }
 
-        $email = $request->input('email');
-        $password = $request->input('password');
+        $token = $request->bearerToken();
 
-        $user = User::where([
-            ['email', $email]
-        ])->first();
+        $auth = UserAuthTokens::whereNull('deleted_at')->where('expired', '!=', true)->where('token', $token)->first();
+        $user = User::find($auth->user_id);
 
-        if (!isset($user) || !Hash::check($password, $user->password)) {
+        if (!$auth || !isset($user)) {
             return response()->json($this->errorMessage(UNAUTHORIZED, UNAUTHORIZED_ACCESS));
         }
 
