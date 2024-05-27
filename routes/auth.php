25bc06a00ebb1f365908b9bcb055fb8dc92ceaf5
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-27 15:22:08 +0300

    Rename users and register controllers properly

--- routes/auth.php
+++ routes/auth.php
@@ -6,8 +6,8 @@ use App\Http\Middleware\CheckAuthorization;
 use App\Http\Middleware\CheckAuthentication;
 use App\Http\Middleware\UserLoginMiddleware;
 use App\Http\Middleware\UserRegisterMiddleware;
-use App\Http\Controllers\v1\Users\LoginController;
-use App\Http\Controllers\v1\Users\RegisterController;
+use App\Http\Controllers\v1\Users\LoginController as UsersLoginController;
+use App\Http\Controllers\v1\Users\RegisterController as UsersRegisterController;
 
 /**
  * Admin authentication routes
@@ -22,19 +22,19 @@ Route::prefix('v1/admin')->group(function () {
 */
 Route::prefix('v1/auth')->middleware([CheckHeaders::class])->group(function () {
     Route::middleware([UserRegisterMiddleware::class])->group(function () {
-        Route::post('/register', [RegisterController::class, 'register']);
+        Route::post('/register', [UsersRegisterController::class, 'register']);
     });
 
-    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/login', [UsersLoginController::class, 'login']);
 });
 
 Route::prefix('v1')->middleware([CheckAuthentication::class, CheckHeaders::class/*, CheckAuthorization::class*/])->group(function () {
     // Authentication Routes
     Route::prefix('auth')->group(function () {
         Route::middleware([UserLoginMiddleware::class])->group(function () {
-            Route::post('/logout', [LoginController::class, 'logout'])
+            Route::post('/logout', [UsersLoginController::class, 'logout'])
                 ->withoutMiddleware([CheckHeaders::class]);
-            Route::post('/refresh-auth-token', [LoginController::class, 'refreshAuthToken'])
+            Route::post('/refresh-auth-token', [UsersLoginController::class, 'refreshAuthToken'])
                 ->withoutMiddleware([CheckHeaders::class]);
         });
     });
