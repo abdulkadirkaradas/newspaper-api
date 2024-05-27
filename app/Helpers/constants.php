32f91f72ef64137202f9352079c72eaf9171ded0
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-05-27 14:25:16 +0300

    Add incorrect content type msg constant

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -4,3 +4,4 @@ const UNAUTHORIZED_ACCESS = 'Unauthorized access';
 const BAD_REQUEST_MSG = "The request could not be understood or was missing required parameters.";
 const SESSION_EXPIRED = 'Session expired';
 const AN_ERROR_OCCURED = 'An unexpected error occured.';
+const INCORRECT_CONTENT_TYPE = 'The request header is incorrect. It should be application/json.';
