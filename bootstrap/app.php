6f5909637a0e13dbc51fa928a172842cf413ab0e
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-05 13:11:11 +0300

    Bind middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             App\Http\Middleware\CheckAuthentication::class,
             App\Http\Middleware\CheckAuthorization::class,
+            App\Http\Middleware\CheckHeaders::class,
+            App\Http\Middleware\CheckUserId::class,
             App\Http\Middleware\UserRegisterMiddleware::class,
             App\Http\Middleware\UserLoginMiddleware::class,
         ]);
