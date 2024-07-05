4e5a98dcf9418c3abbb673361b1fe5ab938e5afb
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-05 15:07:30 +0300

    Add API function that returns logged user news

--- app/Http/Controllers/v1/Users/NewsController.php
+++ app/Http/Controllers/v1/Users/NewsController.php
@@ -2,10 +2,39 @@
 
 namespace App\Http\Controllers\v1\Users;
 
-use App\Http\Controllers\Controller;
+use App\Models\News;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    //
+    /**
+     * Returns logged user news
+     *
+     * @var Request $request
+     * @return array
+     */
+    public function loggedUserNews(Request $request): array
+    {
+        $user = $request->user;
+
+        $news = News::select('id', 'title', 'content', 'created_at')
+            ->with([
+                'newsImages' => function ($query) {
+                    $query->select('user_id', 'news_id', 'name', 'ext', 'fullpath', 'created_at');
+                },
+                'newsReactions' => function ($query) {
+                    $query->select('user_id', 'news_id', 'reaction', 'type', 'created_at');
+                }
+            ])->where('user_id', $user->id)->get();
+
+        return [
+            'id' => $user->id,
+            'name' => $user->name,
+            'lastname' => $user->lastname,
+            'username' => $user->username,
+            'membership_date' => $user->created_at,
+            'news' => $news,
+        ];
+    }
 }
