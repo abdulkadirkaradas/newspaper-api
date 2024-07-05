27c649933575dae944ca3676fa82dd88a1f0e705
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-05 15:14:50 +0300

    Add 'news' API function that return news by id

--- app/Http/Controllers/v1/Users/NewsController.php
+++ app/Http/Controllers/v1/Users/NewsController.php
@@ -37,4 +37,30 @@ class NewsController extends Controller
             'news' => $news,
         ];
     }
+
+
+    /**
+     * Returns news by related id
+     *
+     * @var Request $request
+     * @return array
+     */
+    public function news(Request $request): array
+    {
+        $news = $request->news;
+
+        $news = News::select('id', 'title', 'content', 'created_at')
+            ->with([
+                'newsImages' => function ($query) {
+                    $query->select('user_id', 'news_id', 'name', 'ext', 'fullpath', 'created_at');
+                },
+                'newsReactions' => function ($query) {
+                    $query->select('user_id', 'news_id', 'reaction', 'type', 'created_at');
+                }
+            ])->findOrFail($news->id);
+
+        return [
+            'news' => $news,
+        ];
+    }
 }
