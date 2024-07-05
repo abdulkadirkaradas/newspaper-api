f02575b22f9120a9bb5325fdee90018664ae4436
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-05 13:12:07 +0300

    Add user function

--- app/Http/Controllers/v1/Users/UsersController.php
+++ app/Http/Controllers/v1/Users/UsersController.php
@@ -56,4 +56,40 @@ class UsersController extends Controller
             'role' => $userInfo->roles
         ];
     }
+
+    /**
+     * Returns any user profile informations
+     *
+     * @var Request $request
+     * @return array
+     */
+    public function user(Request $request): array
+    {
+        $user = $request->user;
+
+        $info = User::with([
+            'news' => function ($query) {
+                $query->select('user_id', 'title', 'created_at');
+            },
+            'reactions' => function ($query) {
+                $query->select('user_id', 'reaction_type');
+            },
+            'badges' => function ($query) {
+                $query->select('id', 'user_id', 'name', 'type')
+                      ->with(['badgeImages' => function ($query) {
+                          $query->select('id', 'badge_id', 'fullpath');
+                      }]);
+            },
+        ])->findOrFail($user->id);
+
+        return [
+            'name' => $user->name,
+            'lastname' => $user->lastname,
+            'username' => $user->username,
+            'membership_date' => $user->created_at,
+            'news' => $info->news,
+            'reactions' => $info->reactions,
+            'badges' => $info->badges,
+        ];
+    }
 }
