98bc1ee61e03f3c2e63c4dbb22b959c485ea30c2
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-05 14:36:32 +0300

    Add 'INVALID_NEWS_ID' constant

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -3,6 +3,7 @@
 const UNAUTHORIZED_ACCESS = 'Unauthorized access';
 const BAD_REQUEST_MSG = "The request could not be understood or was missing required parameters.";
 const INVALID_USER_ID = "Invalid User ID";
+const INVALID_NEWS_ID = "Invalid News ID";
 const SESSION_EXPIRED = 'Session expired';
 const AN_ERROR_OCCURED = 'An unexpected error occured.';
 const INCORRECT_CONTENT_TYPE = 'The request header is incorrect. It should be application/json.';
