b7f4acd8e65e818981b70bc81b5cd4694d820790
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-05 14:33:47 +0300

    Improve 'writer' routes structure

--- routes/user.php
+++ routes/user.php
@@ -1,23 +1,23 @@
 <?php
 
-use App\Http\Controllers\v1\Users\NotificationsController;
+use App\Http\Middleware\CheckNewsId;
+use App\Http\Middleware\CheckUserId;
 use App\Http\Middleware\CheckHeaders;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CheckAuthentication;
+use App\Http\Controllers\v1\Users\NewsController;
 use App\Http\Controllers\v1\Users\UsersController;
-use App\Http\Middleware\CheckUserId;
+use App\Http\Controllers\v1\Users\NotificationsController;
 
 /**
  * 'Writer' routes
  */
-Route::prefix('v1')->middleware([CheckAuthentication::class, CheckHeaders::class])->group(function () {
+Route::prefix('v1/writer')->middleware([CheckAuthentication::class, CheckHeaders::class])->group(function () {
 
-    Route::prefix('user')->group(function () {
-        // Returns requested user informations
-        Route::get('/{id}', [UsersController::class, 'user'])->middleware(CheckUserId::class);
-        // Returns logged user informations
-        Route::get('/profile', [UsersController::class, 'profile']);
-    });
+    // Returns requested user informations
+    Route::get('/user/{id}', [UsersController::class, 'user'])->middleware(CheckUserId::class);
+    // Returns logged user informations
+    Route::get('/profile', [UsersController::class, 'profile']);
 
     // All routes support the return of notifications based on a time-range (optional).
     Route::prefix('notifications')->group(function () {
