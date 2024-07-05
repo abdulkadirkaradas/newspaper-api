61ba9543e437381af8ff9f8d59ec3143d4cd4a2c
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-05 15:14:01 +0300

    Bind 'CheckNewsId' middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             App\Http\Middleware\CheckAuthorization::class,
             App\Http\Middleware\CheckHeaders::class,
             App\Http\Middleware\CheckUserId::class,
+            App\Http\Middleware\CheckNewsId::class,
             App\Http\Middleware\UserRegisterMiddleware::class,
             App\Http\Middleware\UserLoginMiddleware::class,
         ]);
