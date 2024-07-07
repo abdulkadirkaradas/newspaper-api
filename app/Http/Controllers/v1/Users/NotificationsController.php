53b10a997347d22bf41fe845822077741031416e
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-07 16:09:39 +0300

    Move notifications function to the UsersController

--- app/Http/Controllers/v1/Users/NotificationsController.php
+++ app/Http/Controllers/v1/Users/NotificationsController.php
@@ -9,41 +9,4 @@ use App\Http\Controllers\Controller;
 
 class NotificationsController extends Controller
 {
-    /**
-     * Returns logged user notifications | all, read, unread | time-range (optional)
-     *
-     * @var Request $request
-     * @return array
-     */
-    public function notifications(Request $request): array
-    {
-        $user = $request->user;
-        $params = $request->only(['type', 'from', 'to']);
-
-        if (count($params) === 0 || !isset($params['type'])) {
-            return CommonFunctions::response(BAD_REQUEST, BAD_REQUEST_MSG);
-        }
-
-        $info = User::with([
-            'notifications' => function ($query) use ($params) {
-                $query->select('user_id', 'type', 'message', 'created_at');
-
-                // Check if 'from' and 'to' parameters exists
-                // and make query by the creation time
-                if (isset($params['from']) || isset($params['to'])) {
-                    $query->whereBetween('created_at', [$params['from'], $params['to']]);
-                }
-
-                // Check if 'type' parameter is not equal 'all' value
-                // and make query by the 'is_read' column
-                if ($params['type'] !== 'all') {
-                    $query->where('is_read', $params['type'] === 'read');
-                }
-            }
-        ])->findOrFail($user->id);
-
-        return [
-            'notifications' => $info->notifications
-        ];
-    }
 }
--- app/Http/Controllers/v1/Users/UsersController.php
+++ app/Http/Controllers/v1/Users/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\v1\Users;
 
+use App\Helpers\CommonFunctions;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -90,6 +91,44 @@ class UsersController extends Controller
             'news' => $info->news,
             'reactions' => $info->reactions,
             'badges' => $info->badges,
+        ];
+    }
+
+        /**
+     * Returns logged user notifications | all, read, unread | time-range (optional)
+     *
+     * @var Request $request
+     * @return array
+     */
+    public function notifications(Request $request): array
+    {
+        $user = $request->user;
+        $params = $request->only(['type', 'from', 'to']);
+
+        if (count($params) === 0 || !isset($params['type'])) {
+            return CommonFunctions::response(BAD_REQUEST, BAD_REQUEST_MSG);
+        }
+
+        $info = User::with([
+            'notifications' => function ($query) use ($params) {
+                $query->select('user_id', 'type', 'message', 'created_at');
+
+                // Check if 'from' and 'to' parameters exists
+                // and make query by the creation time
+                if (isset($params['from']) || isset($params['to'])) {
+                    $query->whereBetween('created_at', [$params['from'], $params['to']]);
+                }
+
+                // Check if 'type' parameter is not equal 'all' value
+                // and make query by the 'is_read' column
+                if ($params['type'] !== 'all') {
+                    $query->where('is_read', $params['type'] === 'read');
+                }
+            }
+        ])->findOrFail($user->id);
+
+        return [
+            'notifications' => $info->notifications
         ];
     }
 
