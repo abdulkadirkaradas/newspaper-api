8c680061321f3209223ccc0821444d46f4779648
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-08 14:44:43 +0300

    Remove ID validation middlewares and add merged middleware

--- app/Http/Middleware/ValidateUserAndNewsIDs.php
+++ app/Http/Middleware/ValidateUserAndNewsIDs.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
-use App\Models\User;
-use Illuminate\Http\Request;
 use App\Helpers\CommonFunctions;
+use App\Models\News;
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CheckUserId
+class ValidateUserAndNewsIDs
 {
     /**
-     * Handle incoming 'User ID' requests.
+     * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
@@ -21,17 +22,17 @@ class CheckUserId
         $id = $request->route('id');
 
         if (!CommonFunctions::checkUUIDValid($id)) {
-            return response()->json(CommonFunctions::response(BAD_REQUEST, INVALID_USER_ID));
+            return response()->json(CommonFunctions::response(BAD_REQUEST, INVALID_ID_NO));
         }
 
-        $user = User::find($id);
+        $instance = str_contains($request->url(), '/post') ? News::find($id) : User::find($id);
 
         // If user couldn't be found, return response message
-        if (!$user) {
-            return response()->json(CommonFunctions::response(BAD_REQUEST, INVALID_USER_ID, 'User ID does not match!'));
+        if (!$instance) {
+            return response()->json(CommonFunctions::response(BAD_REQUEST, INVALID_ID_NO, 'ID does not match!'));
         }
 
-        $request['user'] = $user;
+        $request['model'] = $instance;
 
         return $next($request);
     }
