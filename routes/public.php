55191fb7e3602292894320466d6498495561a255
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-10 18:52:49 +0300

    Rename 'common' route file to 'public'. Adjust route prefixes to the 'public'.

--- routes/public.php
+++ routes/public.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\v1\Users\NewsController;
-use App\Http\Controllers\v1\Users\UsersController;
+use App\Http\Controllers\v1\Public\NewsController;
+use App\Http\Controllers\v1\Public\UserController;
 use App\Http\Middleware\ValidateUserAndNewsIDs;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1/common')->group(function () {
+Route::prefix('v1/public')->group(function () {
     // Returns requested user informations
-    Route::get('/user/{id}', [UsersController::class, 'user'])->middleware(ValidateUserAndNewsIDs::class);
+    Route::get('/user/{id}', [UserController::class, 'user'])->middleware(ValidateUserAndNewsIDs::class);
     // Return logged user news
     Route::get('/post/{id}', [NewsController::class, 'news'])->middleware(ValidateUserAndNewsIDs::class);
 });
