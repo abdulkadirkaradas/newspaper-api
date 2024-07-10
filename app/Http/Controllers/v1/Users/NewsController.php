f449fe45742834ab965b6b6fc77323c0cff07433
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-10 18:51:46 +0300

    Move public 'user' and 'news' functions to the related controllers

--- app/Http/Controllers/v1/Users/NewsController.php
+++ app/Http/Controllers/v1/Users/NewsController.php
@@ -39,32 +39,6 @@ class NewsController extends Controller
         ];
     }
 
-
-    /**
-     * Returns news by related id
-     *
-     * @var Request $request
-     * @return array
-     */
-    public function news(Request $request): array
-    {
-        $news = $request->model;
-
-        $news = News::select('id', 'title', 'content', 'created_at')
-            ->with([
-                'newsImages' => function ($query) {
-                    $query->select('user_id', 'news_id', 'name', 'ext', 'fullpath', 'created_at');
-                },
-                'newsReactions' => function ($query) {
-                    $query->select('user_id', 'news_id', 'reaction', 'type', 'created_at');
-                }
-            ])->findOrFail($news->id);
-
-        return [
-            'news' => $news,
-        ];
-    }
-
     /**
      * Returns all news reactions of logged user
      *
--- app/Http/Controllers/v1/Users/UsersController.php
+++ app/Http/Controllers/v1/Users/UsersController.php
@@ -61,44 +61,6 @@ class UsersController extends Controller
     }
 
     /**
-     * Returns any user profile informations
-     *
-     * @var Request $request
-     * @return array
-     */
-    public function user(Request $request): array
-    {
-        $user = $request->model;
-
-        $info = User::with([
-            'news' => function ($query) {
-                $query->select('user_id', 'title', 'created_at');
-            },
-            'reactions' => function ($query) {
-                $query->select('user_id', 'reaction_type');
-            },
-            'badges' => function ($query) {
-                $query->select('id', 'user_id', 'name', 'type')
-                    ->with([
-                        'badgeImages' => function ($query) {
-                            $query->select('id', 'badge_id', 'fullpath');
-                        }
-                    ]);
-            },
-        ])->findOrFail($user->id);
-
-        return [
-            'name' => $user->name,
-            'lastname' => $user->lastname,
-            'username' => $user->username,
-            'membership_date' => $user->created_at,
-            'news' => $info->news,
-            'reactions' => $info->reactions,
-            'badges' => $info->badges,
-        ];
-    }
-
-    /**
      * Returns logged user notifications | all, read, unread | time-range (optional)
      *
      * @var Request $request
