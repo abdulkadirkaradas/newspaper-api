36d6ebdebd584680bfae5fb09ba081f74d4e3d95
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-10 18:22:31 +0300

    Add 'News' and 'User' controllers for unauth users with different endpoint

---
+++ app/Http/Controllers/v1/Public/NewsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\v1\Public;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * All those functions have been added for unauthenticated users
+ */
+class NewsController extends Controller
+{
+    //
+}
