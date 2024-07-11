0c1408fe1b19e345a67d14cdd9ff02d431c6851a
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-11 14:09:00 +0300

    Add new 'METHOD_NOT_ALLOWED_MSG' constant

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -2,6 +2,7 @@
 
 const UNAUTHORIZED_ACCESS = 'Unauthorized access';
 const BAD_REQUEST_MSG = "The request could not be understood or was missing required parameters.";
+const METHOD_NOT_ALLOWED_MSG = "Method not allowed!";
 const INVALID_ID_NO = "Invalid ID!";
 const SESSION_EXPIRED = 'Session expired';
 const AN_ERROR_OCCURED = 'An unexpected error occured.';
