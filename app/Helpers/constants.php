76cbe994d44934cf8afa943a416ff0cc78ed1067
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-11 15:09:03 +0300

    Add new constants. Add comment lines.

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -1,5 +1,6 @@
 <?php
 
+// API method responses
 const UNAUTHORIZED_ACCESS = 'Unauthorized access';
 const BAD_REQUEST_MSG = "The request could not be understood or was missing required parameters.";
 const METHOD_NOT_ALLOWED_MSG = "Method not allowed!";
@@ -7,3 +8,7 @@ const INVALID_ID_NO = "Invalid ID!";
 const SESSION_EXPIRED = 'Session expired';
 const AN_ERROR_OCCURED = 'An unexpected error occured.';
 const INCORRECT_CONTENT_TYPE = 'The request header is incorrect. It should be application/json.';
+
+// API function returns
+const NEWS_CREATED = "News has been created successfully!";
+const NEWS_CREATION_FAILED = "News could not be created!";
