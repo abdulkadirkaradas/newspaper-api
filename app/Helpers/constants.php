c57d4dd4dd8588c1b8163443ecb6d367d908b949
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-15 16:47:38 +0300

    Add new constants

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -8,8 +8,11 @@ const INVALID_ID_NO = "Invalid ID!";
 const SESSION_EXPIRED = 'Session expired';
 const AN_ERROR_OCCURED = 'An unexpected error occured.';
 const INCORRECT_CONTENT_TYPE = 'The request header is incorrect. It should be application/json.';
+const INCORRECT_IMAGE_UPLOAD_CONTENT_TYPE = 'The request header is incorrect. It should be multipart/form-data.';
 
 // API function returns
 const VALIDATOR_FAILED = "The request could not be understood or was missing required parameters.";
 const NEWS_CREATED = "News has been created successfully!";
 const NEWS_CREATION_FAILED = "News could not be created!";
+const NEWS_IMAGE_CREATED = "News image has been created successfully!";
+const NEWS_IMAGE_CREATION_FAILED = "News image could not be created!";
