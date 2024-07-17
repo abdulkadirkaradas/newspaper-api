ae41f67dd3438f0b64a434540e7f5a166731d296
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-17 14:54:39 +0300

    Add function to return user role name by ID in UserRoles enum file

--- app/Enums/UserRoles.php
+++ app/Enums/UserRoles.php
@@ -6,4 +6,14 @@ enum UserRoles: int {
     case Admin = 1;
     case Moderator = 2;
     case Writer = 3;
+
+    public static function getRole(int $role): ?string
+    {
+        return match ($role) {
+            self::Admin->value => self::Admin->name,
+            self::Moderator->value => self::Moderator->name,
+            self::Writer->value => self::Writer->name,
+            default => null,
+        };
+    }
 }
