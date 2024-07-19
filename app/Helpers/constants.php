349027453438982488b6148781daccc047cbc665
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-19 15:42:39 +0300

    Add new constant

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -12,6 +12,7 @@ const INCORRECT_IMAGE_UPLOAD_CONTENT_TYPE = 'The request header is incorrect. It
 
 // API function returns
 const VALIDATOR_FAILED = "The request could not be understood or was missing required parameters.";
+const USER_NOT_FOUND = "User could not be found!";
 const NEWS_CREATED = "News has been created successfully!";
 const NEWS_CREATION_FAILED = "News could not be created!";
 const NEWS_IMAGE_CREATED = "News image has been created successfully!";
