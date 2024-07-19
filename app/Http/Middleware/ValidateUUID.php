6b9da8f85f44ba70ca579ec197ffef25e89fdae9
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-19 15:58:46 +0300

    Add middleware that validates UUİD is valid

---
+++ app/Http/Middleware/ValidateUUID.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Helpers\CommonFunctions;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateUUID
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $id = $request->input('id');
+
+        if (!CommonFunctions::checkUUIDValid($id)) {
+            return response()
+                ->json(CommonFunctions::response(BAD_REQUEST, INVALID_ID_NO));
+        }
+
+        return $next($request);
+    }
+}
