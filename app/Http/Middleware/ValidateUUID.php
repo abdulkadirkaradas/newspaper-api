439504718eccca727191f9aa919bdaeaeaa1ed51
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-19 16:54:14 +0300

    Add extra validations

--- app/Http/Middleware/ValidateUUID.php
+++ app/Http/Middleware/ValidateUUID.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Helpers\CommonFunctions;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,12 +17,25 @@ class ValidateUUID
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!$request->input('id')) {
+            return $next($request);
+        }
+
         $id = $request->input('id');
 
         if (!CommonFunctions::validateUUID($id)) {
             return response()
                 ->json(CommonFunctions::response(BAD_REQUEST, INVALID_ID_NO));
         }
+
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()
+                ->json(CommonFunctions::response(BAD_REQUEST, USER_NOT_FOUND));
+        }
+
+        $request['providedUser'] = $user;
 
         return $next($request);
     }
