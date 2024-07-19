67a5d88279d8a8bf88961ae152c89253cfba5ca5
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-19 15:52:15 +0300

    Add API function that returns user notifications

--- app/Http/Controllers/v1/Admin/UsersController.php
+++ app/Http/Controllers/v1/Admin/UsersController.php
@@ -108,4 +108,41 @@ class UsersController extends Controller
             ? CommonFunctions::response(SUCCESS, "User has been blocked!")
             : CommonFunctions::response(FAIL, "Failed to block user.");
     }
+
+        /**
+     * Returns logged user notifications | all, read, unread | time-range (optional)
+     *
+     * @var Request $request
+     * @return array
+     */
+    public function notifications(Request $request): array
+    {
+        $params = $request->only(['id', 'type', 'from', 'to']);
+
+        if (count($params) === 0 || !isset($params['id']) || !isset($params['type'])) {
+            return CommonFunctions::response(BAD_REQUEST, BAD_REQUEST_MSG);
+        }
+
+        $info = User::with([
+            'notifications' => function ($query) use ($params) {
+                $query->select('user_id', 'type', 'message', 'created_at');
+
+                // Check if 'from' and 'to' parameters exists
+                // and make query by the creation time
+                if (isset($params['from']) || isset($params['to'])) {
+                    $query->whereBetween('created_at', [$params['from'], $params['to']]);
+                }
+
+                // Check if 'type' parameter is not equal 'all' value
+                // and make query by the 'is_read' column
+                if ($params['type'] !== 'all') {
+                    $query->where('is_read', $params['type'] === 'read');
+                }
+            }
+        ])->find($params['id']);
+
+        return [
+            'notifications' => $info->notifications
+        ];
+    }
 }
