e57755afb063ab0283a2565bba4b7e41a9ed68b4
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-19 16:54:57 +0300

    Improve middleware usage

--- routes/admin.php
+++ routes/admin.php
@@ -7,7 +7,13 @@ use App\Http\Middleware\RoleAdminMiddleware;
 use App\Http\Controllers\v1\Admin\UsersController;
 use App\Http\Middleware\ValidateUUID;
 
-Route::prefix('v1/admin')->middleware([CheckHeaders::class, CheckAuthentication::class, RoleAdminMiddleware::class, 'throttle:30,1'])->group(function () {
+Route::prefix('v1/admin')->middleware([
+    CheckHeaders::class,
+    CheckAuthentication::class,
+    RoleAdminMiddleware::class,
+    ValidateUUID::class,
+    'throttle:30,1'
+])->group(function () {
 
     // User processes
     Route::prefix('user')->group(function () {
@@ -16,21 +22,17 @@ Route::prefix('v1/admin')->middleware([CheckHeaders::class, CheckAuthentication:
         //--------------------------------------------------------------------//
 
         // Return user informations | id, type['all, blocked']
-        Route::get('/', [UsersController::class, 'user'])
-            ->middleware([ValidateUUID::class]);
+        Route::get('/', [UsersController::class, 'user']);
         // Return user notifications|all, read, unread, time-range based
-        Route::get('notifications', [UsersController::class, 'get_user_notifications'])
-            ->middleware([ValidateUUID::class]);
+        Route::get('notifications', [UsersController::class, 'get_user_notifications']);
         // Return user warnings
-        Route::get('warnings', [UsersController::class, 'get_user_warnings'])
-            ->middleware([ValidateUUID::class]);
+        Route::get('warnings', [UsersController::class, 'get_user_warnings']);
 
         //--------------------------------------------------------------------//
         //                          POST Routes                               //
         //--------------------------------------------------------------------//
 
         // Block user
-        Route::post('/block', [UsersController::class, 'block_user'])
-            ->middleware([ValidateUUID::class]);
+        Route::post('/block', [UsersController::class, 'block_user']);
     });
 });
