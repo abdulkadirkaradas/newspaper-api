aa3ef17ee58345674f45b6830d9a93a21affa073
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-19 16:00:21 +0300

    Add 'ValidateUUID' middleware calls to the related routes

--- routes/admin.php
+++ routes/admin.php
@@ -5,17 +5,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CheckAuthentication;
 use App\Http\Middleware\RoleAdminMiddleware;
 use App\Http\Controllers\v1\Admin\UsersController;
+use App\Http\Middleware\ValidateUUID;
 
 Route::prefix('v1/admin')->middleware([CheckHeaders::class, CheckAuthentication::class, RoleAdminMiddleware::class, 'throttle:30,1'])->group(function () {
 
     // User processes
     Route::prefix('user')->group(function () {
         // Return user informations | id, type['all, blocked']
-        Route::get('/', [UsersController::class, 'user']);
+        Route::get('/', [UsersController::class, 'user'])
+            ->middleware([ValidateUUID::class]);
         // Return user notifications|all, read, unread, time-range based
-        Route::get('notifications', [UsersController::class, 'notifications']);
+        Route::get('notifications', [UsersController::class, 'notifications'])
+            ->middleware([ValidateUUID::class]);
 
         // Block user
-        Route::post('/block', [UsersController::class, 'block_user']);
+        Route::post('/block', [UsersController::class, 'block_user'])
+            ->middleware([ValidateUUID::class]);
     });
 });
