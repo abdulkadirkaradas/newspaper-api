0e1eb0c725c10a583533b377244cfc0696a23951
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-27 14:39:06 +0300

    Add 'upload_image' API function to the 'admin/bage' controller

--- app/Http/Controllers/v1/Admin/BadgesController.php
+++ app/Http/Controllers/v1/Admin/BadgesController.php
@@ -39,4 +39,39 @@ class BadgesController extends Controller
 
         return CommonFunctions::response(FAIL, BADGE_CREATION_FAILED);
     }
+
+    /**
+     * Upload badge image
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function upload_image(Request $request): array
+    {
+        $user = $request->user;
+        $badge = $request->badge;
+
+        $validated = CommonFunctions::validateRequest($request, UploadBadgeImageValidator::class);
+
+        if (isset($validated['status']) && $validated['status'] === BAD_REQUEST) {
+            return $validated;
+        }
+
+        // Remove all characters before index 18 and merge IDs
+        $compoundKey = substr($user->id, 19) . '-' . substr($badge->id, 19);
+
+        $fullpath = time() . '_' . $compoundKey . '_' . $validated['name'] . '.' . $request['ext'];
+        $request->image->move(public_path('badges'), $fullpath);
+
+        $newsImage = new BadgeImage();
+        $newsImage->name = $validated['name'];
+        $newsImage->ext = $validated['ext'];
+        $newsImage->fullpath = $fullpath;
+
+        if ($badge->badgeImages()->save($newsImage)) {
+            return CommonFunctions::response(SUCCESS, BADGE_IMAGE_CREATED);
+        } else {
+            return CommonFunctions::response(FAIL, BADGE_IMAGE_CREATION_FAILED);
+        }
+    }
 }
