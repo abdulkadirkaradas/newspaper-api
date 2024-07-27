e4bf8f0f732494475702244fdbc1e7ca67274ebc
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-07-27 14:38:22 +0300

    Add validator for the 'admin/badge/create' function

---
+++ app/Validators/CreateBadgeValidator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Validators;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CreateBadgeValidator
+{
+    public static function validate(Request $request)
+    {
+        $validations = [
+            "name" => ['required', 'string', 'max:50'],
+            "type" => ['required', 'string', 'max:20'],
+            "description" => ['required', 'string', 'max:500'],
+        ];
+
+        $validator = Validator::make($request->all(), $validations);
+
+        if ($validator->fails()) {
+            return [
+                'status' => BAD_REQUEST,
+                'message' => VALIDATOR_FAILED,
+            ];
+        }
+
+        return $validator->validated();
+    }
+}
