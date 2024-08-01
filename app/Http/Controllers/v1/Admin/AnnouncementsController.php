7ea23e0088f5f11c466c5b3f4d73293185d2cfbe
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-08-01 14:11:29 +0300

    Add API function that returns announcements

--- app/Http/Controllers/v1/Admin/AnnouncementsController.php
+++ app/Http/Controllers/v1/Admin/AnnouncementsController.php
@@ -11,6 +11,40 @@ use App\Validators\CreateAnnouncementsValidator;
 class AnnouncementsController extends Controller
 {
     /**
+     * Return announcements | priority, from, to
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function announcements(Request $request): array
+    {
+        $params = $request->only(['priority', 'from', 'to']);
+
+        $announces = Announcements::query();
+
+        if (!empty($params['priority'])) {
+            $announces->where('priority', $params['priority']);
+        }
+
+        if (!empty($params['from']) || !empty($params['to'])) {
+            $from = $params['from'] ?? null;
+            $to = $params['to'] ?? null;
+
+            if ($from && $to) {
+                $announces->whereBetween('created_at', [$from, $to]);
+            } elseif ($from) {
+                $announces->where('created_at', '>=', $from);
+            } elseif ($to) {
+                $announces->where('created_at', '<=', $to);
+            }
+        }
+
+        return [
+            "announcements" => $announces->get()
+        ];
+    }
+
+    /**
      * Create an announcement
      *
      * @param \Illuminate\Http\Request $request
