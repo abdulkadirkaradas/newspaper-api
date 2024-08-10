e71f97d8543bc4ed51df783d50996eb75c8146ed
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-08-10 14:53:10 +0300

    Use 'ValidateUUID' mw insted of 'ValidateUserAndNewsIDs'

--- routes/public.php
+++ routes/public.php
@@ -2,12 +2,12 @@
 
 use App\Http\Controllers\v1\Public\NewsController;
 use App\Http\Controllers\v1\Public\UserController;
-use App\Http\Middleware\ValidateUserAndNewsIDs;
+use App\Http\Middleware\ValidateUUID;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1/public')->group(function () {
+Route::prefix('v1/public')->middleware([ValidateUUID::class])->group(function () {
     // Returns requested user informations
-    Route::get('/user/{id}', [UserController::class, 'user'])->middleware(ValidateUserAndNewsIDs::class);
+    Route::get('/user', [UserController::class, 'user']);
     // Return logged user news
-    Route::get('/post/{id}', [NewsController::class, 'news'])->middleware(ValidateUserAndNewsIDs::class);
+    Route::get('/news', [NewsController::class, 'news']);
 });
