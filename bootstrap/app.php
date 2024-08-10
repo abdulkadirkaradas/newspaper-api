cbf9447ad3c6b5ed4ff458949527ef7b46e225a3
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2024-08-10 15:38:41 +0300

    Fix incorrect authentication check

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(append: [
-            App\Http\Middleware\CheckAuthentication::class,
+            // App\Http\Middleware\CheckAuthentication::class,
             App\Http\Middleware\CheckHeaders::class,
         ]);
         $middleware->alias([
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CheckAuthentication;
 
-Route::prefix('')->middleware([CheckAuthentication::class])->group(function () {
+Route::middleware([CheckAuthentication::class])->group(function () {
     Route::get('/', function () {});
 });
