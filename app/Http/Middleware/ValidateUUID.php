f0fa509022c63e4f833b9adb27de9707d5d4036c
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-02-10 15:05:40 +0300

    Add comment

--- app/Http/Middleware/ValidateUUID.php
+++ app/Http/Middleware/ValidateUUID.php
@@ -13,18 +13,20 @@ use Symfony\Component\HttpFoundation\Response;
 class ValidateUUID
 {
     /**
-     * Handle an incoming request.
+     * This middleware mainly validates UUIDs from incoming requests
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // Take specified parameters
         $params = $request->only(['user_id', 'news_id', 'category_id']);
 
         if (count($params) === 0) {
             return $next($request);
         }
 
+        // Validate UUID parameters
         if (!CommonFunctions::validateUUID($params)) {
             return response()->json(CommonFunctions::response(BAD_REQUEST, INVALID_ID_NO));
         }
@@ -35,6 +37,7 @@ class ValidateUUID
             'newsCategory' => [NewsCategories::class, 'category_id', 'newsCategory', NEWS_CATEGORY_NOT_FOUND],
         ];
 
+        // Check if the models exist and get the model instance, then add it to the request bag
         foreach ($models as [$modelClass, $paramKey, $responseKey, $notFoundMessage]) {
             if (isset($params[$paramKey])) {
                 $model = $modelClass::find($params[$paramKey]);
