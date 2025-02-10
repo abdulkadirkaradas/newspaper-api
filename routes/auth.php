6e9d057c93ed253bb65f0caeffd42cfc778875d3
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-02-10 15:22:38 +0300

    Refactor 'auth' API route names based on REST-API standarts

--- routes/auth.php
+++ routes/auth.php
@@ -10,30 +10,29 @@ use App\Http\Controllers\v1\Users\RegisterController as UsersRegisterController;
 
 /**
  * Admin authentication routes
-*/
+ */
 Route::prefix('v1/admin')->group(function () {
     // Authentication Routes
-    Route::prefix('auth')->group(function () {});
+    Route::prefix('auth')->group(function () { });
 });
 
 /**
  * User authentication routes
-*/
-Route::prefix('v1/auth')->middleware([CheckHeaders::class])->group(function () {
-    Route::middleware([UserRegisterMiddleware::class])->group(function () {
-        Route::post('/register', [UsersRegisterController::class, 'register']);
-    });
-
-    Route::post('/login', [UsersLoginController::class, 'login']);
-});
-
-Route::prefix('v1')->middleware([CheckAuthentication::class, CheckHeaders::class/*, CheckAuthorization::class*/])->group(function () {
-    // Authentication Routes
+ */
+Route::prefix('v1')->middleware([CheckHeaders::class])->group(function () {
     Route::prefix('auth')->group(function () {
-        Route::middleware([UserLoginMiddleware::class])->group(function () {
+        // User registration
+        Route::post('/register', [UsersRegisterController::class, 'register'])
+            ->middleware([UserRegisterMiddleware::class]);
+
+        // User login
+        Route::post('/login', [UsersLoginController::class, 'login']);
+
+        // Protected routes
+        Route::middleware([UserLoginMiddleware::class, CheckAuthentication::class])->group(function () {
             Route::post('/logout', [UsersLoginController::class, 'logout']);
             Route::post('/refresh-auth-token', [UsersLoginController::class, 'refreshAuthToken']);
-            Route::get('/user-information', [UsersLoginController::class, 'userInformation']);
+            Route::get('/user', [UsersLoginController::class, 'userInformation']);
         });
     });
 });
