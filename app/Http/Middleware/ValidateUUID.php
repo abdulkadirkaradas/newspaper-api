023bb2797b84348d71e8ddd685fb4ea26ac2fce4
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-02-12 13:50:56 +0300

    Remove redundant model binding from middleware

--- app/Http/Middleware/ValidateUUID.php
+++ app/Http/Middleware/ValidateUUID.php
@@ -20,7 +20,7 @@ class ValidateUUID
     public function handle(Request $request, Closure $next): Response
     {
         // Take specified parameters
-        $params = $request->only(['user_id', 'news_id', 'category_id']);
+        $params = $request->only(['userId', 'newsId', 'categoryId']);
 
         if (count($params) === 0) {
             return $next($request);
@@ -29,25 +29,6 @@ class ValidateUUID
         // Validate UUID parameters
         if (!CommonFunctions::validateUUID($params)) {
             return response()->json(CommonFunctions::response(BAD_REQUEST, INVALID_ID_NO));
-        }
-
-        $models = [
-            'user' => [User::class, 'user_id', 'providedUser', USER_NOT_FOUND],
-            'news' => [News::class, 'news_id', 'providedNews', NEWS_NOT_FOUND],
-            'newsCategory' => [NewsCategories::class, 'category_id', 'newsCategory', NEWS_CATEGORY_NOT_FOUND],
-        ];
-
-        // Check if the models exist and get the model instance, then add it to the request bag
-        foreach ($models as [$modelClass, $paramKey, $responseKey, $notFoundMessage]) {
-            if (isset($params[$paramKey])) {
-                $model = $modelClass::find($params[$paramKey]);
-
-                if (!$model) {
-                    return response()->json(CommonFunctions::response(BAD_REQUEST, $notFoundMessage));
-                }
-
-                $request[$responseKey] = $model;
-            }
         }
 
         return $next($request);
