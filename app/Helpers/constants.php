f3fecce5b27efea58eb8237e6770d9871486335a
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-02-13 14:57:57 +0300

    Improve invalid id response message

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -4,7 +4,7 @@
 const UNAUTHORIZED_ACCESS = 'Unauthorized access';
 const BAD_REQUEST_MSG = "The request could not be understood or was missing required parameters.";
 const METHOD_NOT_ALLOWED_MSG = "Method not allowed!";
-const INVALID_ID_NO = "Invalid ID!";
+const INVALID_ID_NO = "Invalid UUID Number!";
 const SESSION_EXPIRED = 'Session expired';
 const AN_ERROR_OCCURED = 'An unexpected error occured.';
 const INCORRECT_CONTENT_TYPE = 'The request header is incorrect. It should be application/json.';
