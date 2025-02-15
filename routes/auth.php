ba2585df40901e4e415a14ec439a625e83368301
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-02-15 13:12:18 +0300

    Rename login and register controllers calls in auth route file

--- routes/auth.php
+++ routes/auth.php
@@ -4,8 +4,8 @@ use App\Http\Middleware\CheckHeaders;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CheckAuthentication;
 use App\Http\Middleware\UserRegisterMiddleware;
-use App\Http\Controllers\v1\Users\LoginController as UsersLoginController;
-use App\Http\Controllers\v1\Users\RegisterController as UsersRegisterController;
+use App\Http\Controllers\v1\Users\LoginController;
+use App\Http\Controllers\v1\Users\RegisterController;
 
 /**
  * Admin authentication routes
@@ -20,21 +20,20 @@ Route::prefix('v1/admin')->group(function () {
  */
 Route::prefix('v1')->middleware([
     CheckHeaders::class,
-    'auth:api',
 ])->group(function () {
     Route::prefix('auth')->group(function () {
         // User registration
-        Route::post('/register', [UsersRegisterController::class, 'register'])
+        Route::post('/register', [RegisterController::class, 'register'])
             ->middleware([UserRegisterMiddleware::class]);
 
         // User login
-        Route::post('/login', [UsersLoginController::class, 'login']);
+        Route::post('/login', [LoginController::class, 'login']);
 
         // Protected routes
         Route::middleware([CheckAuthentication::class])->group(function () {
-            Route::post('/logout', [UsersLoginController::class, 'logout']);
-            Route::post('/refresh-auth-token', [UsersLoginController::class, 'refreshAuthToken']);
-            Route::get('/user', [UsersLoginController::class, 'userInformation']);
+            Route::post('/logout', [LoginController::class, 'logout']);
+            Route::post('/refresh-auth-token', [LoginController::class, 'refreshAuthToken']);
+            Route::get('/user', [LoginController::class, 'userInformation']);
         });
     });
 });
